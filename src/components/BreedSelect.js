c01d7de6d93b8650bb091b83eac16f58a784cf69
Maryna-Korbet <m22v86k@gmail.com>
2023-01-07 13:46:39 +0200

    Create BreedSelect.js

---
+++ src/components/BreedSelect.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export class BreedSelect extends Component {
+    state = {
+        breeds: [],
+    };
+    
+    render() {
+        return <div>BreedSelect</div>;
+    }
+}
