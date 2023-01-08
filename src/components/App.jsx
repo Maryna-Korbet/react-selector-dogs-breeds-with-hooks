d9eb4e2dea458f6988ea57f30f0bd7ac7d8060e9
Maryna-Korbet <m22v86k@gmail.com>
2023-01-08 02:01:08 +0200

    added loading  in selectedBreed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,19 +7,32 @@ import { Layout } from "./Layout";
 export class App extends Component {
   state = {
     dog: null,
+    isLoading: false,
+    error: null,
   }
 
   selectedBreed = async breedId => {
     try {
+      this.setState({isLoading: true})
       const dog = await fetchDogByBreed(breedId);
       this.setState({ dog });
-    } catch (error) {}
+    } catch {
+      this.setState({
+        error: "The dog did not boot, please try again."
+      })
+    } finally {
+      this.setState({isLoading: false})
+    }
   }
 
   render() {
+    const { error, dog, isLoading } = this.state;
     return (
       <Layout>
         <BreedSelect onSelect={this.selectedBreed} />
+        {dog && <img src={this.state.dog.url} alt="" width={600}/>}
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        {isLoading && <div>LOADING...</div>}
       </Layout>
     );
   }
