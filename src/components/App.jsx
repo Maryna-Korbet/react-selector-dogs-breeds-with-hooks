77607b166ef5ecc40ca9a39500f3fb7c79c4da46
Maryna-Korbet <m22v86k@gmail.com>
2023-01-08 04:36:04 +0200

    added react-hot-toast

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { fetchDogByBreed } from "api";
 import { Component } from "react";
+import toast, { Toaster } from 'react-hot-toast';
 import { BreedSelect } from "./BreedSelect";
 import { Layout } from "./Layout";
 import { Dog } from "./Dog";
@@ -18,9 +19,7 @@ export class App extends Component {
       const dog = await fetchDogByBreed(breedId);
       this.setState({ dog });
     } catch {
-      this.setState({
-        error: "The dog did not boot, please try again."
-      })
+      toast.error("The dog did not boot, please try again.")
     } finally {
       this.setState({isLoading: false})
     }
@@ -34,6 +33,7 @@ export class App extends Component {
         {dog && !isLoading && <Dog dog={dog} breeds={this.breeds} />}
         {error && <p style={{ color: 'red' }}>{error}</p>}
         {isLoading && <DogSkeleton />}
+        <Toaster position="bottom-right"/>
       </Layout>
     );
   }
