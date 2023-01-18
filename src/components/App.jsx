0f3a68116ee3c687bcbf14291a5dd2159ac257f3
Maryna-Korbet <m22v86k@gmail.com>
2023-01-18 19:46:59 +0200

    updated App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,36 @@
 import { fetchDogByBreed } from "api";
-import { Component } from "react";
-import toast, { Toaster } from 'react-hot-toast';
+import { useState } from "react";
 import { BreedSelect } from "./BreedSelect";
 import { Layout } from "./Layout";
 import { Dog } from "./Dog";
 import { DogSkeleton } from "./DogSkeleton";
 
-export class App extends Component {
-  state = {
-    dog: null,
-    isLoading: false,
-    error: null,
-  }
+const ERROR_MESSAGE = "The dog did not boot, please try again.";
 
-  selectedBreed = async breedId => {
+export function App () {
+    const [dog, setDog] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+  const selectedBreed = async breedId => {
     try {
-      this.setState({ isLoading: true, error: null });
+      setIsLoading(true);
+      setError(null);
       const dog = await fetchDogByBreed(breedId);
-      this.setState({ dog });
+      setDog( dog );
     } catch {
-      toast.error("The dog did not boot, please try again.")
+      setError(ERROR_MESSAGE);
     } finally {
-      this.setState({isLoading: false})
+      setIsLoading(false);
     }
   }
 
-  render() {
-    const { error, dog, isLoading } = this.state;
     return (
       <Layout>
-        <BreedSelect onSelect={this.selectedBreed} />
-        {dog && !isLoading && <Dog dog={dog} breeds={this.breeds} />}
+        <BreedSelect onSelect={selectedBreed} />
+        {dog && !isLoading && <Dog dog={dog} />}
         {error && <p style={{ color: 'red' }}>{error}</p>}
         {isLoading && <DogSkeleton />}
-        <Toaster position="bottom-right"/>
       </Layout>
     );
-  }
 }
